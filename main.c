7d401f286132bd0bb6455dc6373f512a3fbd1b6d
yoonje <elxm6123@gmail.com>
2019-09-25 17:06:49 +0900

    add comment

--- main.c
+++ main.c
@@ -16,11 +16,13 @@ int main(int argc, char *argv[]) {
 
     char filename[BUFFSIZE] = {0};
 
+    // initialize server info
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(SERVERPORT);
 
+    // create server_socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (server_socket == -1) {
@@ -28,11 +30,13 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    // binding server socket using server info
     if (bind(server_socket, (const struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
         printf("Bind Error\n");
         exit(1);
     }
 
+    // listen and queue size initialize 5
     if (listen(server_socket, 5) == -1) {
         printf("Listen Error\n");
         exit(1);
@@ -40,6 +44,7 @@ int main(int argc, char *argv[]) {
 
     while (1) {
 
+        // create client socket and accept
         memset(&client_addr, 0, sizeof(client_addr));
         client_addr_size = sizeof(client_addr);
         client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &client_addr_size);
@@ -50,16 +55,20 @@ int main(int argc, char *argv[]) {
             exit(1);
         }
 
+        // receive file name
         if (recv(client_socket, filename, BUFFSIZE, 0) == -1) {
             printf("Can't receive filename\n");
         }
 
+        // create file
         if ((fp = fopen(filename, "wb")) == NULL) {
             printf("Can't open file\n");
         }
 
+        // receive file using loop
         receive_file(client_socket, fp);
 
+        // check empty file
         if (fp != NULL) {
             fseek(fp, 0, SEEK_END);
             if (ftell(fp) != 0) {
@@ -70,6 +79,7 @@ int main(int argc, char *argv[]) {
                 remove(filename);
             }
         }
+        // flush for stdout buffer
         fflush(stdout);
         close(client_socket);
     }
